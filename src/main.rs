e7bd442965429e88313623610034835fa0b11068
michalszmigiel <michal.szmigiel@tsh.io>
2023-05-25 01:50:12 +0200

    remake deez nuts

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{ self, BufRead, BufReader, Write };
+
+fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
+    let file = File::open(filename).unwrap(); 
+    return io::BufReader::new(file).lines(); 
+}
+
+fn replace(line: &str) -> String {
+    match &line {
+        x if x.contains("PWD=$(shell pwd)") => "PWD=$(shell pwd) \nBRANCH=$(shell basename $(PWD))".to_string(),
+        x if x.contains("NGINX_COMPOSE=-f docker-compose.yml -f") => "NGINX_COMPOSE=-f docker-compose.yml -f ${PWD}/dev/docker-compose.nginx.dev.yml".to_string(),
+        x if x.contains("docker build -t local/$") => "        docker build -t local/${APP_NAME}-${BRANCH}:latest .".to_string(),
+        x if x.contains("USER_UID=${UID} docker-compose ${API_COMPOSE} up -d --force-recreate") => "        USER_UID=${UID} IMAGE_NAME=local/${APP_NAME}-${BRANCH}:latest docker-compose ${API_COMPOSE} up -d --force-recreate".to_string(),
+        _ => line.to_string()
+    }
+}
+ 
+fn main() -> std::io::Result<()> {
+    let lines = read_lines("./Makefile".to_string());
+    let mut file = File::create("Makefile2")?;
+
+    let mut data = vec![];
+    
+    for line in lines {
+        data.push(replace(line?.as_str()));
+    }
+
+    let joined_data = data.join("\n");
+
+    file.write_all(joined_data.as_bytes())?;
+    Ok(())
+}
+

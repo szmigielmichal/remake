bf7cca0d634b1aff76f0ae56d3550d057f029dd6
michalszmigiel <michal.szmigiel@tsh.io>
2023-05-25 08:51:38 +0200

    fix deez nuts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{File, self};
 use std::io::{ self, BufRead, BufReader, Write };
 
 fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
@@ -10,15 +10,15 @@ fn replace(line: &str) -> String {
     match &line {
         x if x.contains("PWD=$(shell pwd)") => "PWD=$(shell pwd) \nBRANCH=$(shell basename $(PWD))".to_string(),
         x if x.contains("NGINX_COMPOSE=-f docker-compose.yml -f") => "NGINX_COMPOSE=-f docker-compose.yml -f ${PWD}/dev/docker-compose.nginx.dev.yml".to_string(),
-        x if x.contains("docker build -t local/$") => "        docker build -t local/${APP_NAME}-${BRANCH}:latest .".to_string(),
-        x if x.contains("USER_UID=${UID} docker-compose ${API_COMPOSE} up -d --force-recreate") => "        USER_UID=${UID} IMAGE_NAME=local/${APP_NAME}-${BRANCH}:latest docker-compose ${API_COMPOSE} up -d --force-recreate".to_string(),
+        x if x.contains("docker build -t local/$") => "\tdocker build -t local/${APP_NAME}-${BRANCH}:latest .".to_string(),
+        x if x.contains("USER_UID=${UID} docker-compose ${API_COMPOSE} up -d --force-recreate") => "\tUSER_UID=${UID} IMAGE_NAME=local/${APP_NAME}-${BRANCH}:latest docker-compose ${API_COMPOSE} up -d --force-recreate".to_string(),
         _ => line.to_string()
     }
 }
  
 fn main() -> std::io::Result<()> {
     let lines = read_lines("./Makefile".to_string());
-    let mut file = File::create("Makefile2")?;
+    let mut file = File::create("Makefile.tmp")?;
 
     let mut data = vec![];
     
@@ -29,6 +29,10 @@ fn main() -> std::io::Result<()> {
     let joined_data = data.join("\n");
 
     file.write_all(joined_data.as_bytes())?;
+
+    fs::remove_file("./Makefile")?;
+    fs::rename("Makefile.tmp", "Makefile")?;
+
     Ok(())
 }
 

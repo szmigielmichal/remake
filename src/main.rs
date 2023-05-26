3ba0c9b46b6bd538685b0b68bf2fa598fb9c1816
michalszmigiel <michal.szmigiel@tsh.io>
2023-05-26 10:16:22 +0200

    refactor deez nuts

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::fs::{File, self};
 use std::io::{ self, BufRead, BufReader, Write };
+use std::path::Path;
 
-fn read_lines(filename: String) -> io::Lines<BufReader<File>> {
-    let file = File::open(filename).unwrap(); 
-    return io::BufReader::new(file).lines(); 
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<BufReader<File>>>
+where P: AsRef<Path>,
+{
+    let file = File::open(filename)?; 
+    Ok(io::BufReader::new(file).lines())
 }
 
 fn process_file(joined_data: String, mut file: File) -> Result<(), std::io::Error> {
@@ -14,28 +17,28 @@ fn process_file(joined_data: String, mut file: File) -> Result<(), std::io::Erro
     Ok(())
 }
 
-fn replace(line: &str) -> String {
-    match &line {
+fn replace(line: String) -> String {
+    match line {
         x if x.contains("PWD=$(shell pwd)") => "PWD=$(shell pwd)\nBRANCH=$(shell basename $(PWD))".to_string(),
         x if x.contains("NGINX_COMPOSE=-f docker-compose.yml -f") => "NGINX_COMPOSE=-f docker-compose.yml -f ${PWD}/dev/docker-compose.nginx.dev.yml".to_string(),
         x if x.contains("docker build -t local/$") => "\tdocker build -t local/${APP_NAME}-${BRANCH}:latest .".to_string(),
         x if x.contains("USER_UID=${UID} docker-compose ${API_COMPOSE} up -d --force-recreate") => "\tUSER_UID=${UID} IMAGE_NAME=local/${APP_NAME}-${BRANCH}:latest docker-compose ${API_COMPOSE} up -d --force-recreate".to_string(),
-        _ => line.to_string()
+        _ => line
     }
 }
  
 fn main() -> std::io::Result<()> {
-    let lines = read_lines("./Makefile".to_string());
-    let file = File::create("Makefile.tmp")?;
+    if let Ok(lines) = read_lines("./Makefile".to_string()) {
+        let file = File::create("Makefile.tmp")?;
 
-    let mut data = vec![];
-    
-    for line in lines {
-        data.push(replace(line?.as_str()));
-    }
+        let mut data = vec![];
+        
+        for line in lines {
+            data.push(replace(line?));
+        }
 
-    process_file(data.join("\n"), file)?;
-
+        process_file(data.join("\n"), file)?;
+    };
     Ok(())
 }
 
